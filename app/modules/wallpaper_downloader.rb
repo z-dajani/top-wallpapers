e3b06828a2d4650177399d406efa3ced24bbfcf4
Remitius <remitius@gmail.com>
2015-12-29 22:53:21 -0600

    Add WallpaperDownloader module and methods to get info about subreddit's top posts

---
+++ app/modules/wallpaper_downloader.rb
@@ -0,0 +1,39 @@
+%w(net/https pry json).each { |f| require f }
+
+module WallpaperDownloader
+  def self.subreddit_top_daily_posts(subreddit_name, amount)
+    if amount.class.ancestors.include?(Numeric) && amount <= 25
+      url = "https://www.reddit.com/r/#{subreddit_name}/top/.json"
+      response = get_json_response(url)
+      posts = response['data']['children'].first(amount)
+      extract_post_info(posts)
+    else
+      raise ArgumentError.new('Invalid post amount: NaN or not in 1..25')
+    end
+  end
+
+  #private class methods
+
+  def self.get_json_response(_url)
+    url = URI.parse(_url)
+    req = Net::HTTP::Get.new(url.to_s)
+    res = Net::HTTP.start(url.host, url.port, use_ssl: true) do 
+      |http| http.request(req)
+    end
+    JSON.parse(res.body)
+  end
+
+  def self.extract_post_info(posts)
+    important_keys = ['author', 'domain', 'permalink', 'score',
+                      'thumbnail', 'title', 'url']
+    posts.map do |p|
+      h = {}
+      important_keys.each do |key|
+        h.merge!(key.to_sym => p['data'][key]) if p['data'][key]
+      end
+      h
+    end
+  end
+  
+  private_class_method :get_json_response, :extract_post_info
+end
