525db74d833e2dd6975e3ecd5fe088a4955ab4e5
Remitius <remitius@gmail.com>
2016-01-02 01:44:55 -0600

    Add link to each ImagePost's permalink on image_posts#index  & add tests for all image_posts#index behavior

--- test/controllers/image_post_controller_test.rb
+++ test/controllers/image_post_controller_test.rb
@@ -6,4 +6,33 @@ class ImagePostControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'index should display image posts when they exist in db' do
+    valid_image_post
+    get :index
+    assert_select '#posts-container', 1
+  end
+
+  test "index shouldn't show image posts container when no posts exist" do
+    get :index
+    assert_select '#posts-container', false
+  end
+
+  test 'index should contain links to the permalink of each post' do
+    p1 = 'https://www.reddit.com/' + valid_image_post.permalink
+    p2 = 'https://www.reddit.com/' + valid_image_post_2.permalink
+    get :index
+    assert_select "a[href='#{p1}']"
+    assert_select "a[href='#{p2}']"
+  end
+
+  test 'index posts should be sorted from highest score to lowest' do
+    valid_image_post
+    bigger_score_post = valid_image_post_2(save: false)
+    bigger_score_post.score = valid_image_post.score + 1
+    bigger_score_post.save
+    permalink = 'https://www.reddit.com/' + bigger_score_post.permalink
+    get :index
+    assert_select ".post:first-of-type a[href='#{permalink}']"
+  end
+
 end
