af44759fa1a248583e10d3040d2019b2892e5628
Remitius <remitius@gmail.com>
2016-01-01 20:04:43 -0600

    Destroy all ImagePosts to prevent random test failures

--- test/unit/wallpaper_downloader_test.rb
+++ test/unit/wallpaper_downloader_test.rb
@@ -4,7 +4,9 @@ require_relative 'wallpaper_downloader_test_data'
 
 
 class WallpaperDownloaderTest < MiniTest::Unit::TestCase
+
   def test_extract_post_info
+    ImagePost.destroy_all
     posts = WallpaperDownloader.send(:extract_post_info, $top_raw_posts)
     attributes = []
     posts.each do |p|
@@ -15,7 +17,7 @@ class WallpaperDownloaderTest < MiniTest::Unit::TestCase
 
   def test_subreddit_top_daily_posts_invalid_subreddit_name
     assert_raises RuntimeError do
-      WallpaperDownloader.subreddit_top_daily_posts('a' * 400, 10)
+      WallpaperDownloader.subreddit_top_daily_posts('a' * 100, 10)
     end
   end
 
