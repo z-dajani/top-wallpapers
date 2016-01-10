63e9f8f6f9688a337b4562e71d732b02f987bf8f
Remitius <remitius@gmail.com>
2016-01-09 23:12:00 -0600

    Use thumb_img attribute of ImagePost for thumbnail images instead of linking to redditmedia

--- app/modules/wallpaper_downloader.rb
+++ app/modules/wallpaper_downloader.rb
@@ -1,6 +1,17 @@
-%w(net/https pry json).each { |f| require f }
+%w(net/https open-uri pry json).each { |f| require f }
 
 module WallpaperDownloader
+
+  def self.refresh_posts
+    subreddits = %w(wallpaper wallpapers earthporn carporn skyporn foodporn
+    abandonedporn mapporn architectureporn roomporn exposureporn)
+    subreddits.each do |sub|
+      old_posts = ImagePost.all.select{ |p| p.subreddit =~ /#{sub}/i }
+      old_posts.each { |p| p.destroy }
+      subreddit_top_daily_posts(sub, 7)
+    end
+  end
+
   def self.subreddit_top_daily_posts(subreddit_name, post_attempts)
     if post_attempts.between?(1, 25)
       url = "https://www.reddit.com/r/#{subreddit_name}/top/.json"
@@ -26,12 +37,16 @@ module WallpaperDownloader
 
   def self.extract_post_info(posts)
     post_info = posts.map do |p|
-      i = ImagePost.new(title: p['data']['title'], url: p['data']['url'],
-                        permalink: p['data']['permalink'],
-                        thumbnail: p['data']['thumbnail'],
-                        subreddit: p['data']['subreddit'],
-                        score: p['data']['score'])
-      i.save ? i : nil
+      ip = ImagePost.new(title: p['data']['title'], url: p['data']['url'],
+        permalink: p['data']['permalink'], thumbnail: p['data']['thumbnail'],
+        subreddit: p['data']['subreddit'], score: p['data']['score'])
+      if ip.save
+        ip.thumb_img = open(ip.thumbnail)
+        ip.save
+        ip
+      else
+        nil
+      end
     end
     post_info.compact
   end
