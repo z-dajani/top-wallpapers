5a27cbee2f3dc2ccb9c3f9dad25a7e95ca597b9c
Remitius <remitius@gmail.com>
2016-02-04 11:11:57 -0600

    Add delayed_job_active_record gem and configure

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'pry-rails'
 gem "paperclip", "~> 4.3"
+gem 'delayed_job_active_record'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160108044926) do
+ActiveRecord::Schema.define(version: 20160204170757) do
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
 
   create_table "image_posts", force: :cascade do |t|
     t.string   "title"
