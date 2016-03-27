9b8f59d3d1c8fc29b83ac406801253a6b0c8e1b5
Remitius <remitius@gmail.com>
2016-03-26 21:12:52 -0500

    Extract ImagePostValidator into its own file

--- app/models/image_post.rb
+++ app/models/image_post.rb
@@ -1,64 +1,5 @@
 %w(net/https open-uri uri json).each { |f| require f }
 
-class ImagePostValidator < ActiveModel::Validator
-  def validate(post)
-    validate_url(post)
-    validate_thumbnail(post)
-    validate_permalink(post)
-    validate_dimensions(post)
-  end
-
-  def validate_url(post)
-    return unless post.url
-
-    unless post.url =~ /\A#{URI::regexp(['http', 'https'])}\z/
-      post.errors[:url] << 'is not a valid http url'
-    end
-
-    parsed_url = URI.parse(post.url)
-    host = parsed_url.host
-
-    unless %w(imgur.com i.imgur.com m.imgur.com).include?(host)
-      post.errors[:url] << 'is not an imgur link'
-    end
-
-    if parsed_url.path.chars.first(3).join == '/a/'
-      post.errors[:url] << 'is an imgur album'
-    end
-  end
-
-  def validate_thumbnail(post)
-    return unless post.thumbnail
-
-    unless post.thumbnail =~ /\A#{URI::regexp(['http', 'https'])}\z/
-      post.errors[:thumbnail] << 'is not a valid http url'
-    end
-
-    host = URI.parse(post.thumbnail).host
-    unless host && host.chars.last(22).join == 'thumbs.redditmedia.com'
-      post.errors[:thumbnail] << 'is not a thumbs.redditmedia link'
-    end
-  end
-
-  def validate_permalink(post)
-    return unless post.permalink
-    unless post.permalink[0..2] == '/r/'
-      post.errors[:permalink] << 'is not in the correct form'
-    end
-  end
-
-  def validate_dimensions(post)
-    if post.height.present? && post.width.nil?
-      post.errors[:width] << 'height was present, width was not'
-    end
-
-    if post.width.present? && post.height.nil?
-      post.errors[:height] << 'width was present, height was not'
-    end
-  end
-
-end
-
 class ImagePost < ActiveRecord::Base
   validates :title, presence: true
   validates :url, presence: true, uniqueness: true
@@ -71,7 +12,7 @@ class ImagePost < ActiveRecord::Base
   validates_with ImagePostValidator
 
   SUBREDDIT_LIST = %w(wallpaper wallpapers earthporn carporn skyporn
-  foodporn abandonedporn mapporn spaceporn cityporn seaporn)
+  foodporn abandonedporn mapporn spaceporn cityporn)
 
 
   def fill_dimensions_from_title
@@ -88,8 +29,7 @@ class ImagePost < ActiveRecord::Base
   ####class methods
 
   def self.refresh_posts(subreddit_count: 11)
-    status = refresh_status
-    return if status == :not_ready 
+    return if refresh_status == :not_ready 
 
     SUBREDDIT_LIST.first(subreddit_count).each do |sub|
       old_posts = ImagePost.all.select{ |p| p.subreddit =~ /#{sub}/i }
